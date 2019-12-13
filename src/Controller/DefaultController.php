d322d427aa95ef39d8cff214c0c137ab615ac558
Luiz Santos <luizcsbh@gmail.com>
2019-12-13 15:32:10 -0300

    Update DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,16 +5,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Mailer\SimpleMailer;
 
 class DefaultController extends AbstractController {
+    /**
+     * @var SimpleMailer
+     */
     private $mailer;
+
+    /**
+     * DefaultController constructor.
+     * @param SimpleMailer $mailer
+     */
+    public function __construct(SimpleMailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
 
     public function index(string $name): Response
     {
         return $this->render('index.html.twig', ['name' => $name, ]);
     }
-    public function __construct(SimpleMailer $mailer){
-        $this->mailer = $mailer;
-    }
-
+   
     public function mail(): Response {
         $this->mailer->send('luizcsbh@gmail.com', 'Enviando mensagem');
         return new Response('<body>ok</body>');
